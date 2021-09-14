bf46666774afe8c42befdd965e4c8a7ca7910c7e
ibsemdias <ibsem.dias@gmail.com>
2021-09-14 16:17:41 -0300

    Correcoes Conexao Banco de Dados

--- gpmi.py
+++ gpmi.py
@@ -10,15 +10,28 @@ app.config['MYSQL_DB'] = "gpmio"
 app.config['MYSQL_PORT'] = 3306
 
 db = MySQL(app)
+        
+SQL_BUSCA_JOGOS = 'SELECT id, nome, descricao from projeto'
 
-def funcaoSoma(a,b):
-    c = a + b
-    return c
+class Projeto:
+    def __init__(self, nome, descricao, id=None):
+        self.id = id
+        self.nome = nome
+        self.descricao = descricao
+
+def traduz_projetos(projetos):
+    def cria_projeto_com_tupla(tupla):
+        return Projeto(tupla[1], tupla[2], id=tupla[0])
+    return list(map(cria_projeto_com_tupla, projetos))
+
 
 @app.route('/')
 def index():
+    cursor = db.connection.cursor()
+    cursor.execute(SQL_BUSCA_JOGOS)
+    listaProjetos = traduz_projetos(cursor.fetchall())
     tituloTabela =  "Lista de Projetos"
-    return render_template('lista.html', titulo = tituloTabela)
+    return render_template('lista.html', titulo = tituloTabela, projetos=listaProjetos)
 
 @app.route('/create')  
 def create():
@@ -32,6 +45,5 @@ def update():
 @app.route('/delete')  
 def delete():
     return render_template('delete.html')
-
 
 app.run(debug=True)
